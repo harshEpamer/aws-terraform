829827b2db7f045ddf67fe61d8261dc7b91d5570
Harshvardhan Singh <harshvardhan_singh@epam.com>
2026-06-01 23:25:36 +0530

    task11

---
+++ task11_migrate_state/terraform.tfvars
@@ -0,0 +1,7 @@
+aws_region = "eu-west-1"
+
+project_id = "cmtr-0g1jc0vy"
+
+policy_name = "resource-move-demo-policy"
+
+policy_description = "Custom role with limited permissions"
---
+++ task11_migrate_state/variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "AWS region where resources are managed"
+  type        = string
+}
+
+variable "project_id" {
+  description = "Project identifier used for tagging and naming"
+  type        = string
+}
+
+variable "policy_name" {
+  description = "Name of the IAM policy managed by this Terraform configuration"
+  type        = string
+}
+
+variable "policy_description" {
+  description = "Description of the IAM policy"
+  type        = string
+}
